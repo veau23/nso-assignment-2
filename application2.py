81a5450bec2425fb750306f2dd8c69b7f59808b8
veau23 <veau23@student.bth.se>
2025-05-03 17:23:44 +0200

    Update application2.py

--- application2.py
+++ application2.py
@@ -13,6 +13,6 @@ def index():
     return Time+" Serving from "+h_name+" ("+IP_addres+")\n"
     
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=5000, debug=True)
+  app.run(host="0.0.0.0",port=80, debug=True)
 
 
